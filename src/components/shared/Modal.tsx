18b75a19db6e62613efc9b4c539049242de410cb
Sudheer Tripathi <tripathisudheer604@gmail.com>
2023-05-22 11:29:53 +0530

    chore: change color

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -1,7 +1,7 @@
 export default function Modal ({modalOpen, setModalOpen, children, modalClasses}: any) {
   return (
     <div className={`modal modal-open ${modalOpen ? 'visible' : 'invisible'}`}>
-      <div className={`modal-box relative bg-slate-700 ${modalClasses}`}>
+      <div className={`modal-box relative bg-white ${modalClasses ? modalClasses :  'bg-slate-700' }`}>
         <label
           htmlFor="my-modal-3"
           className="btn btn-sm btn-circle absolute right-2 top-2"
